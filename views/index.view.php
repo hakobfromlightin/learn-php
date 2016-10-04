c086cf1acd4756d087e136be9a5d2dcd8dc3476a
hakob90 <hakob93baghdasaryan@gmail.com>
2016-10-05 00:44:31 +0400

    view partials

--- views/index.view.php
+++ views/index.view.php
@@ -1,39 +1,13 @@
-<!doctype html>
-<html lang="en">
-<head>
-    <meta charset="UTF-8">
-    <meta name="viewport"
-          content="width=device-width, user-scalable=no, initial-scale=1.0, maximum-scale=1.0, minimum-scale=1.0">
-    <meta http-equiv="X-UA-Compatible" content="ie=edge">
-    <title>Document</title>
-    <style>
-        header {
-            background: #e3e3e3;
-            padding: 2em;
-            text-align: center;
-        }
-    </style>
-</head>
-<body>
-<nav>
-    <ul>
-        <li><a href="/about">About Page</a></li>
-        <li><a href="/about/culture">About Culture</a></li>
-        <li><a href="/contact">Contact Page</a></li>
-    </ul>
-</nav>
-<header>
+<?php require('views/partials/head.view.php'); ?>
     <ul>
         <?php foreach ($tasks as $task): ?>
             <li>
-            <?php if ($task->status == 1): ?>
-                <span style="text-decoration: line-through;"><?= $task->name;  ?></span>
-            <?php else: ?>
-                <span><?= $task->name; ?></span>
-            <?php endif; ?>
+                <?php if ($task->status == 1): ?>
+                    <span style="text-decoration: line-through;"><?= $task->name; ?></span>
+                <?php else: ?>
+                    <span><?= $task->name; ?></span>
+                <?php endif; ?>
             </li>
         <?php endforeach; ?>
     </ul>
-</header>
-</body>
-</html>
+<?php require('views/partials/footer.view.php'); ?>
